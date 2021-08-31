a11f27bb682912ce71e8bb7cc286819d1788a5cb
Nico Rithner <nico@altvia.com>
2021-08-30 18:07:56 -0600

    Complete Building a search feature lesson
    Co-authored-by: Nico nicorithner@gmail.com

--- 4-building-a-search-feature/gif-search/src/App.js
+++ 4-building-a-search-feature/gif-search/src/App.js
@@ -1,44 +1,42 @@
-import React, { Component } from 'react';
-import './App.css';
-import axios from 'axios';
-import SearchForm from './Components/SearchForm';
-import GifList from './Components/GifList';
+import React, { Component } from "react";
+import "./App.css";
+import axios from "axios";
+import SearchForm from "./Components/SearchForm";
+import GifList from "./Components/GifList";
 
 export default class App extends Component {
-  
   constructor() {
     super();
     this.state = {
-      gifs: []
+      gifs: [],
     };
-  } 
-
-  componentDidMount() {
-
   }
-  
+
   performSearch = (query) => {
-    axios.get(`http://api.giphy.com/v1/gifs/search?q=${query}&limit=24&api_key=dc6zaTOxFJmzC`)
-      .then(response => {
+    axios
+      .get(
+        `http://api.giphy.com/v1/gifs/search?q=${query}&limit=24&api_key=dc6zaTOxFJmzC`
+      )
+      .then((response) => {
         this.setState({
-          gifs: response.data.data
+          gifs: response.data.data,
         });
       })
-      .catch(error => {
-        console.log('Error fetching and parsing data', error);
-      });    
-  }
-  
-  render() { 
+      .catch((error) => {
+        console.log("Error fetching and parsing data", error);
+      });
+  };
+
+  render() {
     console.log(this.state.gifs);
     return (
       <div>
         <div className="main-header">
           <div className="inner">
             <h1 className="main-title">GifSearch</h1>
-            <SearchForm onSearch={this.performSearch} />      
-          </div>   
-        </div>    
+            <SearchForm onSearch={this.performSearch} />
+          </div>
+        </div>
         <div className="main-content">
           <GifList data={this.state.gifs} />
         </div>
